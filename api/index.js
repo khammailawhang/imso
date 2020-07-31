3a60801b7f46ea8243e97a0d7b1b472426e6e093
khammailawhang <khammaics@gmail.com>
2020-07-31 17:01:46 +0700

    Changes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-// const bodyParser = require('body-parser')
-// set up port
+    // const bodyParser = require('body-parser')
+    // set up port
 const PORT = 4000
-// app.use(bodyParser.json({limit: '1000000mb'}));
+    // app.use(bodyParser.json({limit: '1000000mb'}));
 app.use('/api', require('./routes/'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

e1774ceb8887b0426e0df6506cb45b8384d9c897
khammailawhang <khammaics@gmail.com>
2020-07-31 16:37:44 +0700

    Changes

--- app/src/components/FeePerMonth.vue
+++ app/src/components/FeePerMonth.vue
@@ -1,8 +1,8 @@
 <script>
-import { Line } from 'vue-chartjs'
+import { Doughnut } from 'vue-chartjs'
 
 export default {
-  extends: Line,
+  extends: Doughnut,
   props: {
     chartdata: {
       type: Object,

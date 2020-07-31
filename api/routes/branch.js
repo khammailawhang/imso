117307b77a2de9805ee2d1e78625904709a82add
khammailawhang <khammaics@gmail.com>
2020-07-31 16:53:44 +0700

    Changes

--- api/routes/branch.js
+++ api/routes/branch.js
@@ -5,19 +5,19 @@ const router = express.Router()
 var knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'localhost',
-      user : 'root',
-      password : '',
-      database : 'imso'
+        host: "127.0.0.1",
+        user: "imso",
+        password: "P@$$w0rd",
+        database: "imso",
     }
-  });
+});
 
-  router.get('/',async function(req,res){
+router.get('/', async function(req, res) {
     let row = await knex('tb_branch');
     res.send({
-      status:true,
-      branchs:row
+        status: true,
+        branchs: row
     })
-  })
+})
 
 module.exports = router
